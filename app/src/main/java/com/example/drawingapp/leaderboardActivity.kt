3d22f028f649b905e03d4c7106156f7b01b7cf6c
wjb035 <127760434+wjb035@users.noreply.github.com>
2025-10-11 18:52:59 -0500

    added the ability to click on a post and bring up a skeleton layout for it

--- app/src/main/java/com/example/drawingapp/leaderboardActivity.kt
+++ app/src/main/java/com/example/drawingapp/leaderboardActivity.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import com.example.drawingapp.R
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -23,15 +24,24 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -54,13 +64,19 @@ fun leaderboard(navCon: NavController) {
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ){
-                Text(text="LEADERBOARD",
+                Text(text="WIP LEADERBOARD",
                     modifier=Modifier,
                     fontSize = 35.sp)
 
             }
         }
     }
+    var showDialog by remember {mutableStateOf(false)}
+    if  (showDialog){
+        AlertDialog(onDismiss = {showDialog = false})
+    }
+
+
     val list = listOf(
         "A", "B", "C", "D"
     ) + ((0..100).map { it.toString() })
@@ -84,7 +100,7 @@ fun leaderboard(navCon: NavController) {
                     Text(text = item)
                 }*/
                 else -> {
-                    IconButton(onClick = {},
+                    IconButton(onClick = {showDialog = true},
                         modifier = Modifier
                             .width(400.dp).height(300.dp)
                     ) {
@@ -100,4 +116,50 @@ fun leaderboard(navCon: NavController) {
     }
 }
 
+@Composable
+fun AlertDialog(onDismiss: ()-> Unit){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {},
+        modifier = Modifier.height(700.dp),
+
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                //Text(text = "Post Details")
+
+            }
+        },
+
+        text = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(text="POST DETAILS", fontSize = 25.sp, color = Color.Black)
+
+                IconButton(onClick = {},
+                    modifier = Modifier
+                        .width(400.dp).height(500.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.image),
+                        contentDescription = "Image",
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                    Button(
+                    onClick = {},
+                    modifier = Modifier.width(180.dp)
+                ){
+                    Text(text="Like Post?")
+                }
+            }
+        }
+
+    )
+
+}
 
