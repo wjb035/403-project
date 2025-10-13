2d28df6c7ddc5a4084d3b281d2eaffa25c2632b7
tax evasion <keahi.temple@gmail.com>
2025-10-12 21:35:54 -0500

    startup screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+
 }
