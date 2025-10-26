98cf6780268713b6d42b4d2cf16bebf8dd3be9dc
tax evasion <keahi.temple@gmail.com>
2025-10-26 15:11:15 -0500

    drawing controller update
    added ability to like and dislike

--- backend/src/main/java/com/basketball/backend/controller/DrawingController.java
+++ backend/src/main/java/com/basketball/backend/controller/DrawingController.java
@@ -4,3 +4,50 @@ import com.basketball.backend.model.Drawing;
 import com.basketball.backend.repository.DrawingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*") // Allow access from Android emulator or any client
+public class DrawingController {
+
+    @Autowired
+    private DrawingRepository drawingRepository;
+
+    // POST /api/users/register
+    // stores a drawing to the user's profile
+    @PostMapping("/storeDrawing")
+    public Drawing storeDrawing(@RequestBody Drawing drawing) {
+        return drawingRepository.save(drawing);
+    }
+
+    @PostMapping("/like/{drawingId}")
+    public Drawing likeDrawing(@PathVariable Long drawingId) {
+        Drawing drawing = drawingRepository.findById(drawingId).orElse(null);
+
+
+        if (drawing == null) {
+            throw new RuntimeException("Drawing not found");
+        }
+
+        drawing.setLikesCount(drawing.getLikesCount().add(1));
+
+        drawingRepository.save(drawing);
+        return drawing
+    }
+
+    @PostMapping("/unlike/{username}")
+    public Drawing unlikeDrawing(@PathVariable Long drawingId) {
+        Drawing drawing = drawingRepository.findById(drawingId).orElse(null);
+
+
+        if (drawing == null) {
+            throw new RuntimeException("Drawing not found");
+        }
+
+        drawing.setLikesCount(drawing.getLikesCount().sub(1));
+
+        drawingRepository.save(drawing);
+        return drawing
+    }
+}
