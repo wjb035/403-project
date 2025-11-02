964034414779c2b60a71dba20061b32c8dfce468
tax evasion <keahi.temple@gmail.com>
2025-11-02 13:45:14 -0600

    testHome and build fixes

--- backend/src/main/java/com/basketball/backend/prompter/WordNetProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.basketball.backend.prompter
-
-import net.sf.extjwnl.dictionary.Dictionary
-
-object WordNetProvider {
-    val dictionary: Dictionary by lazy {
-        Dictionary.getDefaultResourceInstance()
-    }
-}
