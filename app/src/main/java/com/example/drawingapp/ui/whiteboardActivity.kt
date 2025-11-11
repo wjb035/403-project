1965103c416295d4456ee30562cd7e099e1af892
wjb035 <127760434+wjb035@users.noreply.github.com>
2025-11-11 15:33:04 -0600

    updated timer display, and made the drawing time longer- the user can draw for 5 minutes

--- app/src/main/java/com/example/drawingapp/ui/whiteboardActivity.kt
+++ app/src/main/java/com/example/drawingapp/ui/whiteboardActivity.kt
@@ -190,7 +190,12 @@ fun whiteboard(navCon: NavController, userViewModel: UserViewModel) {
                     override fun onTick(millisUntilFinished: Long) {
                         remainingTime = millisUntilFinished
                         val seconds = millisUntilFinished / 1000
-                        countdown = String.format("%02d", seconds % 60)
+                        //if (seconds > 59){
+                        countdown = String.format("%02d:%02d", seconds/60, seconds % 60)
+                        //}
+                        //else {
+                           // countdown = String.format("%02d", seconds % 60)
+                       // }
 
                     }
 
