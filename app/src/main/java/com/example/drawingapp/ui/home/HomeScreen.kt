a78bf3613c679b0233d39783cca9d5c4db299a9e
wjb035 <127760434+wjb035@users.noreply.github.com>
2025-11-11 13:34:26 -0600

    prepared the home screen file for getting the database information for if a user can draw

--- app/src/main/java/com/example/drawingapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/drawingapp/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.drawingapp.ui.home
 
+import android.content.Context
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.media.SoundPool
@@ -20,17 +21,28 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.drawingapp.R
+import com.example.drawingapp.ui.dataStore
 import com.example.drawingapp.ui.whiteboardtheme.WhiteboardSimTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "drawData")
 @Composable
 fun HomeScreen(navCon: NavController) {
     val context = LocalContext.current
-
+    val drawData = booleanPreferencesKey("drawData")
     var soundPool: SoundPool? by remember { mutableStateOf(null) }
     var soundId: Int? by remember { mutableStateOf(null) }
+    var hasDrawn by remember {mutableStateOf(true)}
     LaunchedEffect(context) {
         val attributes = AudioAttributes.Builder()
             .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
@@ -42,7 +54,21 @@ fun HomeScreen(navCon: NavController) {
             .build()
         soundId = soundPool?.load(context, R.raw.deck_ui_default_activation, 1)
     }
+    /*
+    *
+    LaunchedEffect(checked){
+        //incrementCounter(context, drawData, false)
+        canDraw = RetrofitInstance.CanDrawApi.getTime()
+        checked = false
+    }
+    *
+    *
 
+    *
+    LaunchedEffect(Unit) {
+        hasDrawn = HasDrawn(context, drawData)
+    }
+    * */
     WhiteboardSimTheme {
         Box(
             modifier = Modifier
@@ -58,7 +84,22 @@ fun HomeScreen(navCon: NavController) {
 
             Button(onClick = {
                 soundId?.let { soundPool?.play(it, 1f, 1f, 0, 0, 1f) }
+                //if (canDraw && !hasDrawn) {
                 navCon.navigate("whiteboard")
+                //}
+
+                /*
+                *
+                 else {
+                    checked = true
+                    Toast.makeText(
+                        context,
+                        "It's not your turn to draw!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                *
+                * */
             }) {
                 Text("Draw")
             }
@@ -70,4 +111,14 @@ fun HomeScreen(navCon: NavController) {
 @Composable
 fun HomeScreenPreview() {
     HomeScreen(navCon = rememberNavController())
+}
+
+
+suspend fun HasDrawn(context: Context,drawData: Preferences.Key<Boolean>): Boolean{
+    val drawFlow: Flow<Boolean> = context.dataStore.data
+        .map { settings ->
+            // No type safety.
+            settings[drawData] ?: true
+        }
+    return drawFlow.first()
 }
